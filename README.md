d854ae5870faf6229d9489b98d6447e30c608c26
XackiGiFF <m.girsanoff@gmail.com>
2022-11-07 08:38:27 +0300

    First commit v2.12.3

---
+++ README.md
@@ -0,0 +1,16 @@
+# AntiFullNew v2.12.3 for 
+
+## RU
+AntiFullNew - Плагин, основанный на плагине `LimitedCreative 1.0.0 API 1.10.0` (Автор [basprohop](https://github.com/Bassproshops))
+
+Первый выпуск плагина AntiFull произошел 20 октября в 2015 году и стал началом нового форка AntiFull от LimitedCreative, согласно данным первых загрузок, сборок, и датированными файлами.
+
+![image](https://user-images.githubusercontent.com/29034010/200227467-c45c067c-bd3f-421e-8cc8-96657bbda343.png)
+
+## EN
+
+AntiFullNew - Plugin forked `LimitedCreative 1.0.0 API 1.10.0` (by [basprohop](https://github.com/Bassproshops))
+
+The first release of plugin AntiFull starts on 20 oct 2017 year and stay a new fork AntiFull from LimitedCreative (Info by date from files and uploads)
+
+![image](https://user-images.githubusercontent.com/29034010/200228479-5e7af5c6-8516-4cc4-aa41-38d93990d07a.png)
---
+++ src/AntiFull/AntiFullNew.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AntiFull;
+use SQLite3;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class AntiFullNew extends PluginBase implements Listener {
+
+    public $db;
+    private $disableItems = array();
+    public $settings;
+
+    public function onEnable() {
+        $this->saveDefaultConfig();
+        $this->settings = $this->getConfig()->getAll();
+        $disabledItems = $this->getConfig()->get("disabled-items");
+        foreach($disabledItems as $disableItem){
+            $this->disableItems[]=$disableItem;
+        }
+
+        $this->db = new SQLite3($this->getDataFolder() . "blocks.db");
+        $this->db->exec("CREATE TABLE IF NOT EXISTS blocks (name varchar(60), item varchar(60), world varchar(60), location varchar (10000), ip varchar(10000));");
+
+        $this->getServer()->getPluginManager()->registerEvents(new Controll($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function isItemDisabled($item) {
+        return in_array($item, $this->disableItems, true);
+    }
+
+    public function msg($msg) {
+        return TextFormat::GRAY . "[" . TextFormat::GOLD . "AntiFullNew" .
+        TextFormat::GRAY . "] " . TextFormat::WHITE . $msg;
+    }
+}
